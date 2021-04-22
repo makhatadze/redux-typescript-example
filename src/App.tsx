2bda5eb3ab4208ceeaa4c70239ea470ab8c1dc6a
makhatadze <vitomaxatadze@gmail.com>
2021-04-22 13:34:21 +0400

    Create App component.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export class App extends Component<any, any> {
+    render() {
+        return <h1>App</h1>;
+    }
+}
+
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
-
+import App from "./App";
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
