49736ee73306f242abf0cba8c8ed5462ef1baad1
makhatadze <vitomaxatadze@gmail.com>
2021-04-22 13:56:45 +0400

    Make todo reducer with action.

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
+import {todosReducer} from "./todos";
 
 
 export const reducers = combineReducers({
-   counter: () => 1
+   counter: () => 1,
+   todos: todosReducer
 });
