ae46a1a81b45aaa45f13e68a46eb63bcb2ffbb6b
makhatadze <vitomaxatadze@gmail.com>
2021-04-22 13:36:58 +0400

    Create combineReducer and import into our app.

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
-import App from "./App";
+import {App} from "./App";
+import {reducers} from "./reducers";
+
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App color={'green'}/>
+            <App />
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
