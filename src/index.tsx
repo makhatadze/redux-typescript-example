cc036f1bc6a6be2c497926334a82e726980262c7
makhatadze <vitomaxatadze@gmail.com>
2021-04-22 13:27:49 +0400

    Change react component class by functional component.

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,15 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
-    color: string;
+    color?: string;
 }
 
-interface AppState {
-    counter: number;
-}
+const App = (props: AppProps): JSX.Element => {
+    return (
+        <h1>{props.color}</h1>
+    )
+};
 
-class App extends Component<AppProps, AppState> {
-    constructor(props: AppProps) {
-        super(props);
-
-        this.state = {counter: 0}
-    }
-
-    onIncrement = (): void => {
-        this.setState({counter: this.state.counter + 1})
-    }
-
-    onDecrement = (): void => {
-        this.setState({counter: this.state.counter - 1})
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(
     <React.StrictMode>
